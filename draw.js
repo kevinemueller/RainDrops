6c33a251beca06d2e45465ebe466fb8930c9d6e7
Kevin <64878242+kevinemueller@users.noreply.github.com>
2023-03-24 12:27:34 -0500

    Add files via upload

---
+++ draw.js
@@ -0,0 +1,73 @@
+function random(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+var x = 0;
+var y = 0;
+
+function mousePos(event) {
+    x = event.clientX;
+    y = event.clientY;
+}
+
+class RainDrop {
+    constructor() {
+        this.MinSize = 1;
+        this.Maxsize = 5;
+
+        this.MinSpeed = 1;
+        this.MaxSpeed = 10;
+
+        // Opacity
+        this.opacity = Math.random();
+
+        // Set X-Pos
+        this.XPos = random(1, window.innerWidth);
+
+        // Set Size
+        this.size = random(this.MinSize, this.Maxsize);
+
+        // Set Speed
+        this.speed = random(this.MinSpeed, this.MaxSpeed);
+
+        // Set off screen YPos
+        this.YPos = -100;
+    }
+}
+
+var canvas = document.getElementById("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight
+var ctx = canvas.getContext('2d');
+
+// Rain Drops - (total drops)
+var RainDrops = new Array(5000);
+
+// Create and define each raindrop
+for(var i = 0; i < RainDrops.length; i++) {
+    RainDrops[i] = new RainDrop();
+}
+
+function animate() {
+
+    requestAnimationFrame(animate);
+
+    ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+
+    for(var i = 0; i < RainDrops.length; i++) {
+        
+        ctx.fillRect(RainDrops[i].XPos, RainDrops[i].YPos, RainDrops[i].size, RainDrops[i].size);
+        ctx.fillStyle = `rgba(255, 255, 255, ${RainDrops[i].opacity}`;
+
+        // Update Y Pos
+        RainDrops[i].YPos+=RainDrops[i].speed;
+
+        // reset variable when limit is reached
+        if(RainDrops[i].YPos >= window.innerHeight + 100 || (x >= RainDrops[i].XPos - RainDrops[i].size && x <= RainDrops[i].XPos + RainDrops[i].size && y <= RainDrops[i].YPos + RainDrops[i].size && y >= RainDrops[i].YPos - RainDrops[i].size)) {
+            RainDrops[i] = null;
+            RainDrops[i] = new RainDrop();
+        }
+    }
+}
+
+animate();
