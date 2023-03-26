4fd80af1448d6578a67978d170c84905e4a7a12a
Kevin M <64878242+kevinemueller@users.noreply.github.com>
2023-03-26 10:30:49 -0500

    Add files via upload

--- draw.js
+++ draw.js
@@ -1,3 +1,23 @@
+// Cloud Objects
+class Cloud {
+    constructor() {
+
+        // Set Velocity
+        this.Velocity = 0.1;
+
+        // Set Width
+        this.width = window.innerWidth * 0.25;
+        
+        // Set Y-Pos
+        this.YPos = window.innerHeight * 0.25;
+
+        // Set X-Pos
+        this.XPos = random(0, 0);
+        
+    }
+}
+
+
 // Rain Drop Object
 class RainDrop {
     constructor() {
@@ -36,16 +56,17 @@ let y = 0;
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext('2d');
 
-// Rain Drops - (total drops).
+// Rain Drops - (Max Drops)
 let RainDrops = new Array(parseInt(document.getElementById("Rain-Amount").max));
 
-console.log(document.getElementById("Rain-Amount").max);
+// Cloud
+//let Clouds = new Cloud();
 
-// Create and define each raindrop.
+// Create and define each raindrop
 for(let i = 0; i < RainDrops.length; i++)
     RainDrops[i] = new RainDrop();
 
-// Function returns a random int/float between min - max (inclusive).
+// Function returns a random int/float between min - max
 function random(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
@@ -62,7 +83,7 @@ function animate() {
     canvas.width = img.clientWidth;
     canvas.height = img.clientHeight;
 
-    requestAnimationFrame(animate);
+    window.requestAnimationFrame(animate);
 
     // clear canvas
     ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
@@ -71,10 +92,14 @@ function animate() {
     for(let i = 0; i < parseInt(document.getElementById("Rain-Amount").value); i++) {
         
         // Rain Drop Style
-        ctx.fillStyle = `rgba(0, 122, 125, ${RainDrops[i].opacity}`;
+        ctx.fillStyle = `rgba(0, 122, 125, ${RainDrops[i].opacity})`;
 
         // Rain Drop
-        ctx.fillRect(RainDrops[i].XPos, RainDrops[i].YPos, RainDrops[i].size, RainDrops[i].size);
+        //ctx.fillRect(RainDrops[i].XPos, RainDrops[i].YPos, RainDrops[i].size, RainDrops[i].size);
+
+        ctx.beginPath();
+        ctx.arc(RainDrops[i].XPos, RainDrops[i].YPos, RainDrops[i].size, Math.PI, 3*Math.PI);
+        ctx.fill();
 
         // Update Y & X Pos
         RainDrops[i].YPos+=RainDrops[i].speed;
@@ -85,9 +110,30 @@ function animate() {
         // reset variables when limit is reached
         if(RainDrops[i].YPos >= canvas.height + 100 || RainDrops[i].XPos >= img.clientWidth || (x >= RainDrops[i].XPos - RainDrops[i].size && x <= RainDrops[i].XPos + RainDrops[i].size && y <= RainDrops[i].YPos + RainDrops[i].size && y >= RainDrops[i].YPos - RainDrops[i].size)) {
             RainDrops[i] = new RainDrop();
+            //RainDrops[i].XPos = random(Clouds.XPos - 50, Clouds.XPos + Clouds.width + (150/2));
+            //RainDrops[i].YPos = Clouds.YPos;
         }
     }
+    /*
+    // Cloud Style
+    ctx.fillStyle = `rgba(116, 116, 116, 1)`;
+
+    // Cloud
+    ctx.fillRect(Clouds.XPos, Clouds.YPos, Clouds.width, 10);
+
+    ctx.beginPath();
+    ctx.arc(Clouds.XPos, Clouds.YPos + 10, 25, 1*Math.PI, 2*Math.PI);
+    ctx.arc(Clouds.XPos + (Clouds.width / 2), Clouds.YPos + 10, 50, 1*Math.PI, 2*Math.PI);
+    ctx.arc(Clouds.XPos + Clouds.width, Clouds.YPos + 10, 75, 1*Math.PI, 2*Math.PI);
+    ctx.fill();
+
+    Clouds.XPos+=Clouds.Velocity;
+
+    if(Clouds.XPos >= img.clientWidth)
+        Clouds = new Cloud();
+    */
 }
+
 
 // Call animate function
 animate();
