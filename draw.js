3294cee585f5535d215a7667c0bb3a3310ba3177
Kevin M <64878242+kevinemueller@users.noreply.github.com>
2023-03-26 11:16:02 -0500

    Update draw.js

--- draw.js
+++ draw.js
@@ -1,23 +1,3 @@
-// Cloud Objects
-class Cloud {
-    constructor() {
-
-        // Set Velocity
-        this.Velocity = 0.1;
-
-        // Set Width
-        this.width = window.innerWidth * 0.25;
-        
-        // Set Y-Pos
-        this.YPos = window.innerHeight * 0.25;
-
-        // Set X-Pos
-        this.XPos = random(0, 0);
-        
-    }
-}
-
-
 // Rain Drop Object
 class RainDrop {
     constructor() {
@@ -59,9 +39,6 @@ const ctx = canvas.getContext('2d');
 // Rain Drops - (Max Drops)
 let RainDrops = new Array(parseInt(document.getElementById("Rain-Amount").max));
 
-// Cloud
-//let Clouds = new Cloud();
-
 // Create and define each raindrop
 for(let i = 0; i < RainDrops.length; i++)
     RainDrops[i] = new RainDrop();
@@ -82,8 +59,6 @@ function animate() {
     // Set Canvas width & height
     canvas.width = img.clientWidth;
     canvas.height = img.clientHeight;
-
-    window.requestAnimationFrame(animate);
 
     // clear canvas
     ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
@@ -108,30 +83,11 @@ function animate() {
         // reset variables when limit is reached
         if(RainDrops[i].YPos >= canvas.height + 100 || RainDrops[i].XPos >= img.clientWidth || (x >= RainDrops[i].XPos - RainDrops[i].size && x <= RainDrops[i].XPos + RainDrops[i].size && y <= RainDrops[i].YPos + RainDrops[i].size && y >= RainDrops[i].YPos - RainDrops[i].size)) {
             RainDrops[i] = new RainDrop();
-            //RainDrops[i].XPos = random(Clouds.XPos - 50, Clouds.XPos + Clouds.width + (150/2));
-            //RainDrops[i].YPos = Clouds.YPos;
         }
     }
-    /*
-    // Cloud Style
-    ctx.fillStyle = `rgba(116, 116, 116, 1)`;
 
-    // Cloud
-    ctx.fillRect(Clouds.XPos, Clouds.YPos, Clouds.width, 10);
-
-    ctx.beginPath();
-    ctx.arc(Clouds.XPos, Clouds.YPos + 10, 25, 1*Math.PI, 2*Math.PI);
-    ctx.arc(Clouds.XPos + (Clouds.width / 2), Clouds.YPos + 10, 50, 1*Math.PI, 2*Math.PI);
-    ctx.arc(Clouds.XPos + Clouds.width, Clouds.YPos + 10, 75, 1*Math.PI, 2*Math.PI);
-    ctx.fill();
-
-    Clouds.XPos+=Clouds.Velocity;
-
-    if(Clouds.XPos >= img.clientWidth)
-        Clouds = new Cloud();
-    */
+    window.requestAnimationFrame(animate);
 }
-
 
 // Call animate function
 animate();
